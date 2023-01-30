af511b64b57c54dac499391e974dce3e40fa8366
TuanHuynhJK <68528340+humati@users.noreply.github.com>
2023-01-30 22:47:13 +0700

    feat: add sanity api to frontend app

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import sanity from "astro-sanity";
+import sanity from 'astro-sanity';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sanity()]
+  integrations: [
+    tailwind(),
+    sanity({
+      projectId: '7oz2bdcd',
+      dataset: 'production',
+      apiVersion: '2023-01-30',
+      useCdn: false,
+    }),
+  ],
 });
