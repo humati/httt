8efd0dd326d300eba87f2dc6acf7588fef7b7ba4
TuanHuynhJK <68528340+humati@users.noreply.github.com>
2023-01-31 22:50:35 +0700

    style: add avatar and navbar component

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,11 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 import sanity from 'astro-sanity';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
+    svelte(),
     sanity({
       projectId: '7oz2bdcd',
       dataset: 'production',
---
+++ frontend/src/components/Avatar.svelte
@@ -0,0 +1,5 @@
+<div class="avatar">
+  <div class="w-20 rounded-full">
+    <img src="/images/avatar.jpg" alt="avatar"/>
+  </div>
+</div>
